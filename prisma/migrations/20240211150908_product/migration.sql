34be2bca71f8ce9aab30dd8caa6369e9637a6de4
Paphaswalan <paphas.teera@gmail.com>
2024-02-13 15:45:05 +0700

    update2

--- prisma/migrations/20240211150908_product/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `productName` to the `ProductDetail` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "ProductDetail" ADD COLUMN     "productName" TEXT NOT NULL;
