6412a2640b1ea8abaa731824712a7eccd237a81b
Paphaswalan <paphas.teera@gmail.com>
2024-02-16 11:54:28 +0700

    update5

--- niau/next.config.mjs
+++ niau/next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.dummyjson.com',
-      },
-    ],
-    unoptimized:true
-  },
+ // output: 'export',
+   reactStrictMode: true,
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: ' ',
+  //     },
+  //   ],
+  //   unoptimized:true
+  // },
 };
 
 export default nextConfig
--- niau/pages/index.js
+++ niau/pages/index.js
@@ -3,8 +3,12 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/Tryon.module.css";
+import { PrismaClient } from '@prisma/client';
 
-export default function Tryon() {
+const prisma = new PrismaClient();
+
+// ImageUploader component handles the image upload logic
+function ImageUploader() {
     const [imageSrc, setImageSrc] = useState(null);
 
     useEffect(() => {
@@ -13,7 +17,7 @@ export default function Tryon() {
 
         reader.onload = e => {
             setImageSrc(e.target.result);
-        }
+        };
 
         const handleChange = (e) => {
             const file = e.target.files[0];
@@ -29,16 +33,34 @@ export default function Tryon() {
     }, []); // Empty dependency array ensures useEffect runs only once after initial render
 
     return (
-        <>
+        <div className={styles.insertImage}> {/* Class tap at the top */}
+            {/* Your image input and display logic */}
+            <input id="file" type="file" accept="image/*" />
+            {imageSrc && <img id="img" src={imageSrc} alt="Uploaded Image" />}
+        </div>
+    );
+}
+
+export async function getStaticProps() {
+    const colors = await prisma.ProductDetail.findMany();
+    return {
+        props: { colors },
+    };
+}
+
+export default function Tryon({ colors }) {
+    return (
+        <div>
             <Head>
-                <title>Try on | niau</title>
-                <meta name="keywords" content="niau,try on"/>
+                <title>TryOn | niau</title>
             </Head>
-            <div className={styles.tap}> {/* Class tap at the top */}
-                {/* Your image input and display logic */}
-                <input id="file" type="file" accept="image/*" />
-                {imageSrc && <img id="img" src={imageSrc} alt="Uploaded Image" />}
-            </div>
-        </>
-    )
+            <ul>
+                {colors.map((item) => (
+                    <li key={item.productID}>{item.color}</li>
+                ))}
+            </ul>
+            {/* Render the ImageUploader component */}
+            <ImageUploader />
+        </div>
+    );
 }
